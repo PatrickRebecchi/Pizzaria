364fe59624b44795ee90d3cf13f25c91d43d8144
Patrick <patrickrebecchi@hotmail.com>
2023-11-25 18:38:31 -0300

    criando pack da pizzaria

---
+++ src/com/projeto/pizzaria/Cardapio.java
@@ -0,0 +1,18 @@
+package com.projeto.pizzaria;
+
+public class Cardapio {
+    public static String [] obterCardapio(){
+        String[] cardapio = {
+            "Pizza Margherita",
+            "Pizza Pepperoni",
+            "Pizza Calabresa",
+            "Pizza Quatro Queijos",
+            "Pizza Frango com Catupiry",
+            "Pizza Portuguesa",
+            "Pizza Vegetariana",
+            "Pizza Doce de Chocolate",
+            "Pizza de Banana com Canela"
+        };
+        return cardapio;
+    }
+}
